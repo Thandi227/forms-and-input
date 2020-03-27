c6bf87ab4298565fa27c6aa807306ffc29182322
Thandi <tandeewa@gmail.com>
2020-03-27 14:39:40 -0400

    2 commit

--- src/App.js
+++ src/App.js
@@ -1,18 +1,4 @@
 import React from 'react';
 import './App.css';
 
-class App extends React.Component() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-     
-      </header>
-    </div>
-  );
-}
-
-export default App;
+class App extends React.Component() 
